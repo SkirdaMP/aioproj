17b7bb311d703a5c6de0b041b0427d250f670ad0
SkirdaMP <skirdamp@gmail.com>
2020-04-05 19:29:13 +0300

    first proj version

--- aioproj/app.py
+++ aioproj/app.py
@@ -1,7 +1,14 @@
 from aiohttp import web
+import aiohttp_jinja2
+import jinja2
+
 from .routes import setup_routes
 
 async def create_app():
     app = web.Application()
+    aiohttp_jinja2.setup(
+    	app,
+    	loader = jinja2.PackageLoader('aioproj', 'templates')
+    )
     setup_routes(app)
     return app
--- aioproj/views/frontend.py
+++ aioproj/views/frontend.py
@@ -1,4 +1,6 @@
 from aiohttp import web
+from aiohttp_jinja2 import template
 
+@template('index.html')
 async def index(request):
-    return web.Response(text='Hi')
+    return {}
