cefd9e30d9795b868eeebe04b000acd546104a61
SkirdaMP <skirdamp@gmail.com>
2020-04-26 21:40:37 +0300

    New version

--- aioproj/db.py
+++ aioproj/db.py
@@ -5,11 +5,21 @@ from sqlalchemy import (
 
 __all__ = ('Post_model', )
 
-meta = MetaData()
+meta_post = MetaData()
 Post_model = Table(
-    'post', meta,
+    'post', meta_post,
     Column('id', Integer, primary_key=True),
     Column('title', VARCHAR(255), nullable = True),
     Column('g_url', VARCHAR, ),
     Column('save_url', VARCHAR, )
+)
+
+meta_login = MetaData()
+
+Login_model = Table(
+    'login', meta_login,
+    Column('id', Integer, primary_key=True),
+    Column('username', VARCHAR(255), index = True, unique = True, ),
+    Column('email', VARCHAR, index = True, unique = True, ),
+    Column('password_hash', VARCHAR, )
 )
